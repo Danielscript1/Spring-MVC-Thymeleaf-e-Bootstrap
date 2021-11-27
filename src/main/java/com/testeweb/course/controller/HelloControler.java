6014d2a1293b0e031fa7d3de5d4c08ecf4f76fe0
Danielscript1 <danielsousagit@gmail.com>
2021-11-27 12:09:07 -0300

    O primeiro_Controller_mandando_informacoes_para_view

---
+++ src/main/java/com/testeweb/course/controller/HelloControler.java
@@ -0,0 +1,17 @@
+package com.testeweb.course.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloControler {
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("nome", "ola mundo");
+		return "hello";
+		
+	}
+}
