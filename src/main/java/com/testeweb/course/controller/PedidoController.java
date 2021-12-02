a47498614a33a8532a530283a92b005d5724128f
Danielscript1 <danielsousagit@gmail.com>
2021-12-02 14:24:23 -0300

    MostrandoMensagemErroBootstrap

--- src/main/java/com/testeweb/course/controller/PedidoController.java
+++ src/main/java/com/testeweb/course/controller/PedidoController.java
@@ -8,10 +8,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.testeweb.course.dto.RequisicaoNovoPedido;
 import com.testeweb.course.model.Pedido;
 import com.testeweb.course.services.PedidoService;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("pedido")
@@ -26,15 +28,19 @@ public class PedidoController {
 		
 	}
 	 @RequestMapping(method = RequestMethod.POST, value="novo")
-	public String novo(@Valid RequisicaoNovoPedido requisicao,  BindingResult result) {
+	public String novo(@Valid RequisicaoNovoPedido requisicao,  BindingResult result,RedirectAttributes attributes) {
 		if(result.hasErrors()) {
-			return "pedido/formulario";
+			attributes.addFlashAttribute("flashMessage", "Verifique os campos!");
+			attributes.addFlashAttribute("flashType", "danger");
+			return "redirect:/pedido/formulario";
 		}
 			
 		
 		Pedido pedido = requisicao.toPedido();
 		pedidoService.salvar(pedido);
-		return "pedido/formulario";
+		attributes.addFlashAttribute("flashMessage", "Ususario Cadastrado com  sucesso!");
+		attributes.addFlashAttribute("flashType", "success");
+		return "redirect:/pedido/formulario";
 		
 	}
 }
